5c955bd16484c9e8f507cfe992bf99e5ebfcfb9d
Marcin Patalon <marcin.patalon@jeppesen.com>
2021-01-05 11:41:14 +0100

    [init-cpp-project] cmake config, vsc config, etc.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <functional>
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+int main() {
+  std::vector myvector{3, 1, 2, 9, 4, 8, 7, 5, 6};
+  auto greater_than_five = [](int i) { return i > 5; };
+  auto next_number = [](int i) { return i + 1; };
+
+  auto filtered = myvector | std::ranges::views::filter(greater_than_five) |
+                  std::views::transform(next_number);
+
+  for (auto i : filtered) {
+    std::cout << i << ' ';
+  }
+
+  return 0;
+}
