c4f8680177dddb8fe2ce7f5eb395edc609c26829
Marcin Patalon <marcin.patalon@jeppesen.com>
2021-01-07 10:24:40 +0100

    [init-cpp-project] gtest ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <ranges>
 #include <vector>
+#include <string>
 
 int main() {
+  std::string s;
   std::vector myvector{3, 1, 2, 9, 4, 8, 7, 5, 6};
   auto greater_than_five = [](int i) { return i > 5; };
   auto next_number = [](int i) { return i + 1; };
