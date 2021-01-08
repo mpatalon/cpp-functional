b77b9cd175e40308d5594d02243f59e1855fc7b5
Marcin Patalon <marcin.patalon@jeppesen.com>
2021-01-08 11:56:09 +0100

    [init-cpp-project] simplify configuration

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,15 @@
 #include <vector>
 #include <string>
 
+
 int main() {
   std::string s;
+
   std::vector myvector{3, 1, 2, 9, 4, 8, 7, 5, 6};
+  for (auto i : myvector) {
+    std::cout << i << ' ';
+  }
+
   auto greater_than_five = [](int i) { return i > 5; };
   auto next_number = [](int i) { return i + 1; };
 
